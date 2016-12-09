b4f66eb006aeabe280bd28a5d0a083331e8338fa
truongd8593 <dangtruong080593@gmail.com>
2016-12-09 21:43:53 +0700

    Add files via upload

---
+++ sum.f90
@@ -0,0 +1,29 @@
+real*8 function function_sum(fsize,fvec)
+ 
+    integer fsize,i
+    real*8 fvec(fsize)
+    real*8 sum
+ 
+    function_sum=0.0
+ 
+    do i=1,fsize
+      function_sum=function_sum+fvec(i)  
+    end do
+ 
+    return
+ 
+end 
+ 
+subroutine subroutine_sum(fsize,fvec,sum)
+ 
+    integer fsize,i
+    real*8 fvec(fsize)
+    real*8 sum
+ 
+    sum=0.0
+ 
+    do i=1,fsize
+      sum=sum+fvec(i)
+    end do
+ 
+end
